a2fc755f2a924681018ad7f306123910c08ff410
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2017-12-28 14:26:00 -0600

    uploading files

--- kukulkan-files/src/main/java/mx/dads/infotec/kukulkan/RemoveHeader.java
+++ kukulkan-files/src/main/java/mx/dads/infotec/kukulkan/RemoveHeader.java
@@ -1,23 +1,24 @@
 package mx.dads.infotec.kukulkan;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.FileVisitResult;
 import java.nio.file.FileVisitor;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
+import java.nio.file.StandardOpenOption;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public final class RemoveHeader {
 
-	private static final Pattern HEADER_PATTERN = Pattern.compile("(^<%#[\\s\\w-:.//,()\":;]*-%>)(<%#[\\s\\w-:.//,()\":;]*-%>)",Pattern.DOTALL);
-
 	public static void main(String... aArgs) throws IOException {
-		// String ROOT = "/home/daniel/Desktop/migration/test";
-		String ROOT = "/home/daniel/Desktop/migration/test/spring-boot";
+		String ROOT = "/home/daniel/git/migration/kukulkan-files/src/main/resources";
 		FileVisitor<Path> fileProcessor = new ProcessFile();
 		Files.walkFileTree(Paths.get(ROOT), fileProcessor);
 	}
@@ -25,20 +26,45 @@ public final class RemoveHeader {
 	private static final class ProcessFile extends SimpleFileVisitor<Path> {
 		@Override
 		public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-			if (file.getFileName().toString().equals("_build.gradle")) {
-				String content = new String(Files.readAllBytes(file));
-
-				Matcher matcher = HEADER_PATTERN.matcher(content);
-				if (matcher.find()) {
-					System.out.println(matcher.group(2));
+			String sContent = null;
+			try (BufferedReader br = new BufferedReader(new FileReader(file.toFile()))) {
+				String line;
+				String firstLine = br.readLine();
+				if (firstLine != null && firstLine.matches("^<%#[\\w\n]*")) {
+					while (!(line = br.readLine()).matches("-%>")) {
+					}
+					StringBuilder content = new StringBuilder();
+					while ((line = br.readLine()) != null) {
+						content.append(line).append("\n");
+					}
+					sContent = content.toString();
+					saveToFile(file, sContent);
+					System.out.println("changed: " + file);
+				} else {
 				}
-				// String content = new String(Files.readAllBytes(file));
-				// System.out.println(content.replaceAll("^<%#[\\s\\w-:.//,()\":;]*-%>",
-				// ""));
-				return FileVisitResult.TERMINATE;
 			}
 
 			return FileVisitResult.CONTINUE;
+		}
+	}
+
+	private static boolean saveToFile(Path pathToSave, String content) {
+		createDirectories(pathToSave);
+		try (final BufferedWriter out = Files.newBufferedWriter(pathToSave, StandardCharsets.UTF_8,
+				StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)) {
+			out.write(content);
+			return true;
+		} catch (IOException ioe) {
+			return false;
+		}
+	}
+
+	private static boolean createDirectories(Path path) {
+		try {
+			Files.createDirectories(path.getParent());
+			return true;
+		} catch (IOException e) {
+			return false;
 		}
 	}
 }
